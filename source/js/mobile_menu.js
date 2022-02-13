89b2fdac0b23ead2477181dff58cdf57f835a0eb
A1exanderVasilyev <alexandrvasilev12rus@gmail.com>
2022-02-13 21:00:39 +0300

    adaptive markup (full)

--- source/js/mobile_menu.js
+++ source/js/mobile_menu.js
@@ -1,31 +1,7 @@
-/*let burger = document.querySelector('.menu_burger');
-let nav = document.querySelector('.menu');
-console.log(burger);
-console.log(nav);
+let burgerButton = document.querySelector('.nav-toggle');
+let navBar = document.querySelector('.nav');
 
-function mobileMenuOn() {
-    burger.addEventListener('click', () => {
-        let newMenu = nav.classList.toggle('.mobile_menu');
-        console.log(newMenu);
-        burger.addClass('.open_menu')
-        
-    })
-}*/
-
-/*window.onload = function() {
-    let mainMenu = document.querySelector('.menu');
-    let burger = document.querySelector('.menu_burger');
-    burger.addEventListener('click', function() {
-        console.log(mainMenu);
-        console.log(burger);
-        burger.toggleClass('open');
-        mainMenu.toggleClass('mobile_menu');
-        
-})
-}*/
-
-$(document).ready(function() {
-    $('.menu_burger').click(function(event) {
-        $('.menu_burger, .menu').toggleClass('active')
-    })
+burgerButton.addEventListener('click', function() {
+    burgerButton.classList.toggle('active');
+    navBar.classList.toggle('mobile-nav');
 })
